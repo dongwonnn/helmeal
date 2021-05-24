53e100be73c8db2662054d5f08339d7420d1877c
dongwon <kkum9408@gmail.com>
2021-05-24 19:21:22 +0900

    [#15] Update Chart.js inner Text

--- src/components/Common/DoughnutGraph.js
+++ src/components/Common/DoughnutGraph.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
-const data = {
-  maintainAspectRatio: false,
-  responsive: false,
-  datasets: [
-    {
-      // 합은 50임.
-      data: [36, 14],
-      backgroundColor: ['rgba(255, 139, 120, 1)', 'rgba(245, 245, 245, 1)'],
-      //   borderRadius: 20,
-      cutout: '75%',
-    },
-  ],
-};
+import './DoughnutGraph.scss';
 
-const DoughnutGraph = () => {
-  return <Doughnut data={data} />;
+const DoughnutGraph = ({ ingredient, amount }) => {
+  const data = {
+    maintainAspectRatio: false,
+    responsive: false,
+    datasets: [
+      {
+        data: [amount, 50 - amount],
+        backgroundColor: ['rgba(255, 139, 120, 1)', 'rgba(245, 245, 245, 1)'],
+        borderRadius: 10,
+        cutout: '75%',
+      },
+    ],
+  };
+
+  return (
+    <div className="GraphContainer">
+      <div className="TextContainer">
+        <p>{amount}</p>
+        <p>{ingredient}</p>
+      </div>
+      <Doughnut data={data} />
+    </div>
+  );
 };
 
 export default DoughnutGraph;
--- src/pages/MenuDetailPage.js
+++ src/pages/MenuDetailPage.js
@@ -4,6 +4,8 @@ import { useLocation } from 'react-router';
 import LargeMeal_02 from '../assets/images/large/Large_Meal_02.png';
 import DoughnutGraph from '../components/Common/DoughnutGraph';
 import { ReactComponent as FishProtein } from '../assets/images/ingredients/Fish_Protein.svg';
+import Button from '../components/Common/Button';
+import { Link } from 'react-router-dom';
 
 const MenuDetailPage = () => {
   const { pathname } = useLocation();
@@ -12,37 +14,11 @@ const MenuDetailPage = () => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  const onClick = (e) => {
-    console.log(e);
-    console.log(e.target);
-  };
-
   return (
     <div className="MenuDetailPage">
       <div className="MenuDetailContainer">
         <div className="MenuCategories">
           <ul>
-            <li className="MenuImg">
-              <div></div>
-            </li>
-            <li className="MenuImg">
-              <div></div>
-            </li>
-            <li className="MenuImg">
-              <div></div>
-            </li>
-            <li className="MenuImg">
-              <div></div>
-            </li>
-            <li className="MenuImg">
-              <div></div>
-            </li>
-            <li className="MenuImg">
-              <div></div>
-            </li>
-            <li className="MenuImg">
-              <div></div>
-            </li>
             <li className="MenuImg">
               <div></div>
             </li>
@@ -71,7 +47,7 @@ const MenuDetailPage = () => {
           <div className="MenuDetail">
             <h3>헬밀P 프로틴</h3>
             <h2>데리야끼 연어 스테이크와 그린빈 볶음 그리고 퀴노아</h2>
-            <div className="MenuIntegrity" onClick={onClick}>
+            <div className="MenuIntegrity">
               <div className="MenuCalorie">
                 <p className="Calorie">510</p>
                 <p className="CalorieText">칼로리</p>
@@ -84,16 +60,19 @@ const MenuDetailPage = () => {
             </div>
             <div className="MenuNutrient">
               <div className="DoughnutContainer">
-                <DoughnutGraph />
+                <DoughnutGraph ingredient="단백질" amount="36" />
               </div>
               <div className="DoughnutContainer">
-                <DoughnutGraph />
+                <DoughnutGraph ingredient="지방" amount="15" />
               </div>
               <div className="DoughnutContainer">
-                <DoughnutGraph />
+                <DoughnutGraph ingredient="탄수화물" amount="10" />
               </div>
             </div>
           </div>
+          <Link to="/subscribe-option">
+            <Button>루틴 시작하기</Button>
+          </Link>
         </div>
       </div>
     </div>
