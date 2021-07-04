a6f553b8dac10080ed00dd36813a7199ed08c0b1
dongwon <kkum9408@gmail.com>
2021-07-04 21:04:42 +0900

    [#54] loading.js -> ts 전환

--- src/reducers/loading.ts
+++ src/reducers/loading.ts
@@ -1,18 +1,23 @@
-const START_LOADING = 'loading/START_LOADING';
-const FINISH_LOADING = 'loading/FINISH_LOADING';
+const START_LOADING = 'loading/START_LOADING' as const;
+const FINISH_LOADING = 'loading/FINISH_LOADING' as const;
 
-export const startLoading = (requestType) => ({
+export const startLoading = (requestType: string) => ({
   type: START_LOADING,
   requestType,
 });
-export const finishLoding = (requestType) => ({
+export const finishLoding = (requestType: string) => ({
   type: FINISH_LOADING,
   requestType,
 });
 
+// 액션 타입
+type LoadingAction =
+  | ReturnType<typeof startLoading>
+  | ReturnType<typeof finishLoding>;
+
 const initialStete = {};
 
-function loading(state = initialStete, action) {
+function loading(state = initialStete, action: LoadingAction) {
   switch (action.type) {
     case START_LOADING:
       return {
